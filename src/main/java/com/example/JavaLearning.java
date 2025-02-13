f3909a53d5f908e339181b902ac0f95dd44f7d0e
FreNzQuiN <mfazagtg@gmail.com>
2025-02-13 11:36:15 +0700

    new file:   pom.xml 	new file:   src/main/java/com/example/JavaLearning.java 	new file:   target/classes/com/example/JavaLearning.class

---
+++ src/main/java/com/example/JavaLearning.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ *
+ * @author muham
+ */
+
+public class JavaLearning {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
