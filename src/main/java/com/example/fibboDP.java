d6a2ebc2549deaf7f52a4ded746e39be9123763c
FreNzQuiN <mfazagtg@gmail.com>
2025-02-20 15:19:13 +0700

    Add FIbbonaci using DP

---
+++ src/main/java/com/example/fibboDP.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import java.util.HashMap;
+
+public class fibboDP {
+    static HashMap<Integer, Integer> memo = new HashMap<>();
+
+    static int fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        if (memo.containsKey(n)) {
+            return memo.get(n);
+        }
+        int result = fib(n - 1) + fib(n - 2);
+        memo.put(n, result);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int result = fib(10);
+        System.out.println(result);
+    }
+}
